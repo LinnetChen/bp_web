984dac9980ca0a9db61de638fa521c04e256c6b0
Linnet <linnet_chen@digeam.com>
2024-01-08 09:59:56 +0800

    v1.0.3

--- resources/views/front/prereg.blade.php
+++ resources/views/front/prereg.blade.php
@@ -467,7 +467,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="js/front/vue_data.js?v=1.25"></script>
+    <script src="js/front/vue_data.js?v=1.26"></script>
     <script src="js/front/view.js?v=1.2"></script>
     <script src="js/front/swiper.js?v=1.1"></script>
     <script src="js/front/main.js?v=1.1"></script>
